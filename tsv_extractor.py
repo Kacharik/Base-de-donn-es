c2c9ca84f69d3515b5d908cb86f4c42664fb54f0
Kacharik <karthik.achari@outlook.com>
2024-05-18 17:24:45 +0200

    Rajouter une fonction check_data

--- tsv_extractor.py
+++ tsv_extractor.py
@@ -24,6 +24,46 @@ def create_connection():
 #################################################################################################
 # OTHER FUNCTIONS
 ################################################################################################
+
+def check_data(comment_type, review_text, rating, date_comment, recommendation, restaurant_name, isdelivery, d_h_rating, visit_date, items_ordered, cost, start, end, reason, client_id):
+    if comment_type == "deleted":
+        return (
+            isinstance(review_text, str) and 
+            isinstance(rating, int) and
+            isinstance(date_comment, datetime) and
+            isinstance(recommendation, str) and 
+            isinstance(restaurant_name, str) and 
+            isinstance(isdelivery, int) and 
+            isinstance(d_h_rating, int) and 
+            isinstance(visit_date, datetime) and 
+            isinstance(items_ordered, list) and all(isinstance(item, str) for item in items_ordered) and 
+            isinstance(cost, float) and 
+            isinstance(start, int) and 
+            isinstance(end, int) and 
+            isinstance(reason, str) and 
+            isinstance(client_id, int)
+        )
+    elif comment_type == "valid":  #Doesnt have the reason for deletion
+        return (
+            isinstance(review_text, str) and 
+            isinstance(rating, int) and
+            isinstance(date_comment, datetime) and
+            isinstance(recommendation, str) and 
+            isinstance(restaurant_name, str) and 
+            isinstance(isdelivery, int) and 
+            isinstance(d_h_rating, int) and 
+            isinstance(visit_date, datetime) and 
+            isinstance(items_ordered, list) and all(isinstance(item, str) for item in items_ordered) and 
+            isinstance(cost, float) and 
+            isinstance(start, int) and 
+            isinstance(end, int) and 
+            isinstance(client_id, int)
+        )
+    else :
+        print("Comment type not properly specified")
+        return False
+    
+
 def sql_get_id(connection, table, id, column, value):
     """
     Create an SQL request to get an id of a row in a table.
@@ -101,13 +141,13 @@ def extract_deleted_reviews(file_path, connection):
             #Les valeurs apres le else sont pour voir si ca marche ou pas
             review_text = fields[0] 
             rating = int(float(fields[1])) if len(fields[1]) == 1 else -1
-            date_comment = parse_date(fields[2]) if len(fields[2]) == (13 or 14 or 15 or 16 or 17)  else '9999-12-31 00:00:01'
+            date_comment = parse_date(fields[2]) if len(fields[2]) == (13 or 14 or 15 or 16 or 17)  else datetime.strptime("9999-12-31 00:00:01", "%Y-%m-%d %H:%M:%S")
             print(date_comment)
             recommendation = fields[3] if "éviter" in fields[3] or "recommandé" in fields[3] else 'INCORRECT VALUE'
             restaurant_name = fields[4] 
             isdelivery = 0 if fields[5][1] == "H" else 1 # H pour Hospitality
             delivery_hospitality_rating = int(fields[5][-1])
-            visit_date = parse_date(fields[6]) if len(fields[6]) == (8 or 9 or 10)  else '9999-12-31'
+            visit_date = parse_date(fields[6]) if len(fields[6]) == (8 or 9 or 10)  else datetime.strptime("9999-12-31", "%Y-%m-%d")
             items_ordered = fields[7].split(";") 
             cost = float(fields[8]) if float(fields[8]) else 9999.99
             start_time = int(fields[9]) if len(fields[9]) == 1 or len(fields[9]) == 2 else 24
@@ -115,28 +155,25 @@ def extract_deleted_reviews(file_path, connection):
             reviewer_name = fields[11]
             reason = fields[12]
             client_id = sql_get_id(connection, "Client", "idCLient","nom", reviewer_name)
-            if client_id is None:
-                print(f"No client found with name: {reviewer_name}")
-            else:
-                print(f"Client ID: {client_id}")
+            
+            if check_data("deleted", review_text, rating, date_comment,recommendation, restaurant_name, isdelivery, delivery_hospitality_rating, visit_date, items_ordered, cost, start_time, end_time, reason, client_id):
+                # Define the columns and corresponding values for insertion
 
-            # Define the columns and corresponding values for insertion
+                columns = [
+                    "Client", "restaurant", "recommandation", "DateAvis", "commentaire",
+                    "DateExp", "HeureDebut","HeureFin", "PrixTotal", "Cote", "Isdelivery",
+                    "CoteFeeling", "raison"
+                ]
+                # remplacer 100 par id du client (avec son nom)
+                values = [
+                    client_id, restaurant_name, recommendation, visit_date, review_text,
+                    date_comment, start_time, end_time, cost, rating, isdelivery,
+                    delivery_hospitality_rating, reason
+                ]
 
-            columns = [
-                "Client", "restaurant", "recommandation", "DateAvis", "commentaire",
-                "DateExp", "HeureDebut","HeureFin", "PrixTotal", "Cote", "Isdelivery",
-                "CoteFeeling", "raison"
-            ]
-            # remplacer 100 par id du client (avec son nom)
-            values = [
-                client_id, restaurant_name, recommendation, visit_date, review_text,
-                date_comment, start_time, end_time, cost, rating, isdelivery,
-                delivery_hospitality_rating, reason
-            ]
-
-            # Insert into AvisRefuse table
-            print("hello thereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee 3")
-            insert_into_table(connection, "AvisRefuse", columns, values)
+                # Insert into AvisRefuse table
+                print("hello thereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee 3")
+                insert_into_table(connection, "AvisRefuse", columns, values)
 
 
 def extract_valid_reviews(file_path, connection):
@@ -151,13 +188,13 @@ def extract_valid_reviews(file_path, connection):
             #Les valeurs apres le else sont pour voir si ca marche ou pas
             review_text = fields[0] 
             rating = int(float(fields[1])) if len(fields[1]) == 1 else -1
-            date_comment = parse_date(fields[2]) if len(fields[2]) == (13 or 14 or 15 or 16 or 17)  else '9999-12-31 00:00:01'
+            date_comment = parse_date(fields[2]) if len(fields[2]) == (13 or 14 or 15 or 16 or 17)  else datetime.strptime("9999-12-31 00:00:01", "%Y-%m-%d %H:%M:%S")
             print(date_comment)
             recommendation = fields[3] if ("éviter" or "recommandé") in fields[3] else 'INCORRECT VALUE'
             restaurant_name = fields[4] 
             isdelivery = 0 if fields[5][1] == "H" else 1 # H pour Hospitality
             delivery_hospitality_rating = int(fields[5][-1])
-            visit_date = parse_date(fields[6]) if len(fields[6]) == (8 or 9 or 10)  else '9999-12-31'
+            visit_date = parse_date(fields[6]) if len(fields[6]) == (8 or 9 or 10)  else datetime.strptime("9999-12-31", "%Y-%m-%d")
             items_ordered = fields[7].split(";") 
             cost = float(fields[8]) if float(fields[8]) else 9999.99
             start_time = int(fields[9]) if len(fields[9]) == 1 or len(fields[9]) == 2 else 24
@@ -166,27 +203,25 @@ def extract_valid_reviews(file_path, connection):
             client_id = sql_get_id(connection, "Client", "idCLient","nom", reviewer_name)
             if client_id is None:
                 print(f"No client found with name: {reviewer_name}")
-            else:
-                print(f"Client ID: {client_id}")
+            
+            if check_data("valid", review_text, rating, date_comment,recommendation, restaurant_name, isdelivery, delivery_hospitality_rating, visit_date, items_ordered, cost, start_time, end_time, client_id):
+                # Define the columns and corresponding values for insertion
 
+                columns = [
+                    "Client", "restaurant", "recommandation", "DateAvis", "commentaire",
+                    "DateExp", "HeureDebut","HeureFin", "PrixTotal", "Cote", "Isdelivery",
+                    "CoteFeeling",
+                ]
+                # remplacer 100 par id du client (avec son nom)
+                values = [
+                    client_id, restaurant_name, recommendation, visit_date, review_text,
+                    date_comment, start_time, end_time, cost, rating, isdelivery,
+                    delivery_hospitality_rating,
+                ]
 
-            # Define the columns and corresponding values for insertion
-
-            columns = [
-                "Client", "restaurant", "recommandation", "DateAvis", "commentaire",
-                "DateExp", "HeureDebut","HeureFin", "PrixTotal", "Cote", "Isdelivery",
-                "CoteFeeling",
-            ]
-            #remplacer 100 par l'id du client
-            values = [
-                client_id, restaurant_name, recommendation, visit_date, review_text,
-                date_comment, start_time, end_time, cost, rating, isdelivery,
-                delivery_hospitality_rating, 
-            ]
-
-            # Insert into AvisRefuse table
-            print("hello thereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee 3")
-            insert_into_table(connection, "AvisValid", columns, values)
+                # Insert into AvisRefuse table
+                print("hello thereeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee 3")
+                insert_into_table(connection, "AvisRefuse", columns, values)
 
             
 
