271a9a0fe835145bcfd490ac9427327fa7fa21d3
Glazelof <117599865+Glazelof@users.noreply.github.com>
2024-05-20 17:33:53 +0200

    Requete 2 du Restaurateur

--- Menu/Restaurateur.py
+++ Menu/Restaurateur.py
@@ -110,4 +110,35 @@ def New_resto(cursor):
 
 
 def Avis_resto(cursor):
-    pass
+    good_choice = False
+    restaurant = ""
+    while(not good_choice):
+        cursor.execute("SELECT restaurant FROM Restaurant")
+        resultat = cursor.fetchall()
+        print("Choisissez un restaurant")
+        print("------------------------")
+        print()
+        for ligne in resultat:
+            print(ligne[0])
+        print()
+        restaurant = input()
+        effacer_terminal()
+        for ligne in resultat:
+            if(restaurant == ligne[0]):
+                good_choice = True
+
+    good_choice = False
+    while(not good_choice):
+        cursor.execute("SELECT * FROM AvisValid WHERE restaurant = '" + restaurant + "'")
+        resultat = cursor.fetchall()
+        print("Voici tout les Avis")
+        print("-------------------")
+        print()
+        for ligne in resultat:
+            print(ligne)
+        print()
+        print("Inscrivez 'back' pour retourner en arrière")
+        choix = input()
+        effacer_terminal()
+        if(choix == 'back'):
+            good_choice = True
