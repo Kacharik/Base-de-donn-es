0c9d64397e139673a1ca04babaf1090e5ca207dc
celagucam <celagucam@us.es>
2024-05-21 16:03:33 +0200

    requête 3

--- Menu/Requete_3.py
+++ Menu/Requete_3.py
@@ -1,2 +1,21 @@
+import mysql.connector
+
+
 def Requete3(cursor):
-    pass
+    query = """
+        SELECT c.nom, COUNT(av.IdAvis) AS total_plats_mexicanos
+        FROM Client c
+        JOIN AvisValid av ON c.idClient = av.Client
+        JOIN Restaurant r ON av.restaurant = r.restaurant
+        WHERE r.TypeResto = 'mexicain'
+        GROUP BY c.nom
+        ORDER BY total_plats_mexicanos DESC
+        LIMIT 10;
+    """
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    print("The top 10 clients who have consumed the most Mexican dishes are:")
+    for client in results:
+        print(f"{client[0]} - Total Mexican dishes: {client[1]}")
+
