154f26af706d132fb5f90839f8a240b61ae30e6c
Glazelof <117599865+Glazelof@users.noreply.github.com>
2024-05-23 00:39:17 +0200

    Requete numéro 5

--- Menu/Requete_5.py
+++ Menu/Requete_5.py
@@ -1,2 +1,20 @@
 def Requete5(cursor):
-    pass
+    query = """
+    SELECT 
+        city,
+        zipcode,
+        AVG(evaluation) AS average_evaluation
+    FROM 
+        Restaurant
+    GROUP BY 
+        city, zipcode
+    ORDER BY 
+        average_evaluation ASC
+    LIMIT 1;
+    """
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    print("Le code postal de la ville dans laquelle les restaurants sont les moins bien notés en moyenne est:")
+    for client in results:
+        print(f"{client[1]}")
